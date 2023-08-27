dce2658d702901f800799514d650cce5847f18e8
Selma Simsek <selmasimsek0406@gmail.com>
2023-08-27 14:57:42 +0200

    v1

---
+++ src/main/java/varıables/Varıables01.java
@@ -0,0 +1,30 @@
+package varıables;
+
+public class Varıables01 {
+    public static void main(String[] args) {
+        char ilkHarf = 'S';
+        System.out.println(ilkHarf);
+
+        boolean ögrencimisin = true;
+        System.out.println(ögrencimisin);
+
+        byte sinif = 7;
+        System.out.println(sinif);
+
+        short okulMevcudu = 1200;
+        System.out.println(okulMevcudu);
+
+        int ülkeNufusu = 80000000;
+        System.out.println(ülkeNufusu);
+
+        long sacTeliSayisi = 555555555555555L;
+        System.out.println(sacTeliSayisi);
+
+        float cantaFiyati = 29.99f;
+        System.out.println(cantaFiyati);
+
+        double hucreAgirligi = 0.000000000012;
+        System.out.println(hucreAgirligi);
+
+    }
+}
